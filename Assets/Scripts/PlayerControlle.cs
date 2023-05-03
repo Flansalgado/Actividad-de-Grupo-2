e1f70e87d8dbf6f22a2a2de78b3f72db3f9242e1
Joel Salgado <jojouniprofe@gmail.com>
2023-05-03 16:26:03 -0500

    subida ejercicio en clase

--- Assets/Scripts/PlayerControlle.cs
+++ Assets/Scripts/PlayerControlle.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerControlle : MonoBehaviour
 {
     // Private variable
-    private float speed = 5.0f;
-    private float turnSpeed = 25.0f;
+    public float speed = 5.0f;
+    public float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
 
         // Start is called before the first frame update
     void Start()
@@ -20,13 +24,20 @@ public class PlayerControlle : MonoBehaviour
     void Update()
     {
         // This is where we get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
         
         //Move the vehicule forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // We turn the vehicule
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        //
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
 
     }
 }
